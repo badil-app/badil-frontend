538f123de710f4a6eb728f27d15bf357510aaa9f
khanabdu25 <khanabdu@umich.edu>
2024-03-10 12:00:49 -0400

    add splash

--- BadilRN/Stack.tsx
+++ BadilRN/Stack.tsx
@@ -2,12 +2,18 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./Home";
 import Catalogue from "./Catalogue";
 import ProductDetail from "./ProductDetail";
+import SplashScreen from "./Splash";
 
 const Stack = createNativeStackNavigator();
 
 export default function MyStack() {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator initialRouteName="Splash">
+            <Stack.Screen
+                name="Splash"
+                component={SplashScreen}
+                options={{ headerShown: false }}
+            />
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Catalogue" component={Catalogue} />
             <Stack.Screen name="ProductDetail" component={ProductDetail} />
